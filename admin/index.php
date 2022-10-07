99a2e6a90d46a543e8ae0425b26890d624bc1f41
johnpatrick-it <johnpatrickhaguimit@gmail.com>
2022-10-08 06:39:31 +0800

    first commit

---
+++ admin/index.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title>Food Order Website - Hope Page</title>
+
+        <link = rel = "stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!--- Menu Section Starts -->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+    
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Admin</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Food</a></li>
+                    <li><a href="#">Order</a></li>
+                    
+                </ul>
+            </div>
+        </div>
+        <!--- Menu Section Ends -->
+        
+        <!--- Main Content Section Starts -->
+        <div class="main-content">
+            <div class="wrapper">
+            <strong>DASHBOARD</strong>
+
+            <div class="col-4"></div>
+                <h1>5</h1>
+                Categories
+            </div>
+        </div>
+        <!--- Main Content Section Ends -->
+
+        <!--- Footer Section Starts -->
+        <div class="footer">
+            <div class="wrapper">
+            <p class="text-center">2022 All rights reserved, Quezon City University Restaurant. Developed By - SBIT3Q Group 3</p>
+            </div>
+        </div>
+        <!--- Footer Section Ends -->
+    </body>
+</html>
