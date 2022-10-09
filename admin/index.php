326571b2aec269d815306ed6f05dcaf81022f9c0
johnpatrick-it <johnpatrickhaguimit@gmail.com>
2022-10-09 18:23:51 +0800

    https://github.com/johnpatrick-it/web-design-course-restaurant-master.git

--- admin/index.php
+++ admin/index.php
@@ -1,30 +1,8 @@
-<html>
-    <head>
-        <title>Food Order Website - Hope Page</title>
-
-        <link = rel = "stylesheet" href="../css/admin.css">
-    </head>
-    <body>
-        <!--- Menu Section Starts -->
-        <div class="menu text-center">
-            <div class="wrapper">
-                <ul>
-    
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Admin</a></li>
-                    <li><a href="#">Category</a></li>
-                    <li><a href="#">Food</a></li>
-                    <li><a href="#">Order</a></li>
-                    
-                </ul>
-            </div>
-        </div>
-        <!--- Menu Section Ends -->
-        
+<?php include('partials/menu.php'); ?>   
         <!--- Main Content Section Starts -->
         <div class="main-content">
             <div class="wrapper">
-                <strong>DASHBOARD</strong>
+                <h1>DASHBOARD</h1>
 
                 <div class="col-4 text-center">
                     <h1>5</h1>
@@ -56,12 +34,4 @@
         </div>
         <!--- Main Content Section Ends -->
 
-        <!--- Footer Section Starts -->
-        <div class="footer">
-            <div class="wrapper">
-            <p class="text-center">2022 All rights reserved, Quezon City University Restaurant. Developed By - SBIT3Q Group 3</p>
-            </div>
-        </div>
-        <!--- Footer Section Ends -->
-    </body>
-</html>
+<?php include('partials/footer.php') ?>      
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+ <!--- Footer Section Starts -->
+ <div class="footer">
+            <div class="wrapper">
+            <p class="text-center">2022 All rights reserved, Quezon City University Restaurant. Developed By - SBIT3Q Group 3</p>
+            </div>
+        </div>
+        <!--- Footer Section Ends -->
+    </body>
+</html>
